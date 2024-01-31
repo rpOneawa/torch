160c8a5c4f847a17a8706b16a763dca05997ad0c
rpONE <rpone@rpone.top>
2024-01-31 16:53:49 +0800

    保持亮屏

--- app/src/main/java/com/rpone/torch/MainActivity.kt
+++ app/src/main/java/com/rpone/torch/MainActivity.kt
@@ -9,18 +9,16 @@ import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
-    private var isBlack = true
+    private lateinit var textView: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         supportActionBar?.hide()
 
-        // 设置状态栏为透明
         window.addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
-        // 让布局延伸到状态栏
         window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
 
-        val textView = TextView(this).apply {
+        textView = TextView(this).apply {
             text = "点击打开"
             textSize = 24f
             setTextColor(Color.WHITE)
@@ -31,15 +29,17 @@ class MainActivity : AppCompatActivity() {
         setContentView(textView)
         textView.setBackgroundColor(Color.BLACK)
 
+        // 设置保持屏幕亮度
+        window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
+
         textView.setOnClickListener {
-            if (isBlack) {
+            if (textView.text.isNotEmpty()) {
                 textView.text = ""
                 textView.setBackgroundColor(Color.WHITE)
             } else {
                 textView.text = "点击打开"
                 textView.setBackgroundColor(Color.BLACK)
             }
-            isBlack = !isBlack
         }
     }
 
@@ -47,6 +47,9 @@ class MainActivity : AppCompatActivity() {
         super.onResume()
         // 设置亮度为最大
         window.attributes = window.attributes.apply { screenBrightness = 1f }
+        // 切换到黑屏
+        textView.text = "点击打开"
+        textView.setBackgroundColor(Color.BLACK)
     }
 
     override fun onPause() {
